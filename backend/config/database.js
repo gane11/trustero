49b968313b8e850267b0ac4ee2c06fc88ae1c33c
aleksandar <aca.djordjevic1113@gmail.com>
2021-03-10 15:34:41 -0800

    working on the backend

---
+++ backend/config/database.js
@@ -0,0 +1,21 @@
+const config = require('./index');
+
+const db = config.db;
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host;
+
+
+module.export = {
+    development: {
+        username,
+        password,
+        database,
+        host,
+        dialect: 'postgres',
+    },
+    // production: {
+
+    // }
+}
